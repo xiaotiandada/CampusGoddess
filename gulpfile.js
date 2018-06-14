eaf801e87f25a81c80f4ea71d83b9882345a4108
xiaotiandada <952822399@qq.com>
2018-06-14 18:44:09 +0800

    初始化工具和环境

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,44 @@
-const gulp = require('gulp')
-const connect = require('gulp-connect')
+var gulp = require('gulp')
+var connect = require('gulp-connect')
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnext = require('postcss-cssnext');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+
 
 gulp.task('connect', function(){
-  connect.server({
-    root: 'app',
-    port: 8000,
-    livereload: true
-  })
+    connect.server({
+        root: 'dist',
+        port: 8000,
+        livereload: true
+    })
 })
 
 gulp.task('html', function(){
-  gulp.src('./app/*.html')
-    .pipe(connect.reload())
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist/'))
+        .pipe(connect.reload())
 })
 
-gulp.task('css', function(){
-  gulp.src('./app/css/*.css')
-    .pipe(connect.reload())
-})
+gulp.task('css', function () {
+    var plugins = [
+        cssnext,
+        autoprefixer()
+    ]
+    return gulp.src('app/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload())
+});
+
 
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html'])
-  gulp.watch(['./app/css/*.css'], ['css'])
+    gulp.watch(['./app/*.html'], ['html'])
+    gulp.watch(['./app/css/*.css'], ['css'])
 })
 
 gulp.task('default', ['connect','watch'])
